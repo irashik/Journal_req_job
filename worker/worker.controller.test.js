b42e84565d9cb70786b32baa1cf952a9b75f48e1
Dmitrii Irashin <irashin.dim@gmail.com>
2020-05-25 19:38:18 +0300

    testing worker.controller

--- worker/worker.controller.test.js
+++ worker/worker.controller.test.js
@@ -17,7 +17,7 @@ const request          = require('supertest');
 
 const controller       = require('./worker.controller');
 
-const Worker                        = require('./worker.model');
+const Worker           = require('./worker.model');
 
 
 
@@ -26,7 +26,7 @@ chai.use(chaiAsPromised);
 
 describe("Test модуля worker.controller", function() {
     
-     describe.only('Тестирование метода create', () => {
+     describe('Тестирование метода create', () => {
          
          afterEach(() => {
               
@@ -128,6 +128,7 @@ describe("Test модуля worker.controller", function() {
                  controller.created(req, res);
                      
                  should(res.render.calledOnce).be.true;
+                 
                  should(res.render.firstCall.args[1]).containDeep(respond);
                  
              
@@ -138,22 +139,10 @@ describe("Test модуля worker.controller", function() {
      
     
     
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+    //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     
     describe('Тестирование метода index', () => {
-               
         
-         it ('Стандартное поведение', function() {
              /*
               * обращаемся к методу
               * метод должен обратиться к базе данных
@@ -161,66 +150,249 @@ describe("Test модуля worker.controller", function() {
               * 
               */
              
+              afterEach(() => {
+              
+                 Worker.FindAllWorker.restore();
+                 
              
-             controller.index(res, req);
-             
-             
-             
-             
-         });
-    });
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-     describe('Тестирование метода saved', () => {
-               
+             });
         
-         it ('Стандартное поведение', function() {
+          describe("Testing method 1 - return ok + data", () => {
+        
+            before(() => {
+                 
+                 const data = { name: 'name' };
+                 const err = null;
+                                
+                 const stub = sinon
+                            .stub(Worker, 'FindAllWorker')
+                            .yields(data, err);
+                   
+                 });
+
+
+                it ('Стандартное поведение', function() {
+                 /*
+                  * обращаемся к методу
+                  * метод должен обратиться к базе данных
+                  * и метод должен вернуть данные из базы -курсор и статус сообщения - ок или ошибка.
+                  * // надо проверить исполнение ответа http с передачей параметра
+                  */
+
+                 let status = 'ok';
+                 let data = { name: 'name' };
+                 let respond = { data: data, status: status };
+                 let req = { };
+                                  
+                 let res = {
+                     render: sinon.spy()
+                 };
+                 
+                 controller.index(req, res);
+                 
+                 
+                 should(res.render.calledOnce).be.true;
+                 should(res.render.firstCall.args[1]).containDeep(respond);
+                    
+                 });
+
+             });
+         
+
+          describe("Testing create method 2", () => {
+         
+             before(() => {
+                     const err = 'error';
+                     const data = {};
+                     const stub = sinon
+                            .stub(Worker, 'FindAllWorker')
+                            .yields(data, err);
+                    
+             });
+         
+               it ('возвращается error  из БД - поведение', function() {
              /*
               * обращаемся к методу
               * метод должен обратиться к базе данных
               * и метод должен вернуть данные из базы -курсор и статус сообщения - ок или ошибка.
               * 
               */
+                         
+                 let req = { };
+                 let res = {
+                     render: sinon.spy()
+                 };
+                 
+                 const respond = { data: null, status: "Error response Database" };
              
              
-             controller.saved(res, req);
+             controller.index(req, res);
+                      
+          
+             should(res.render.calledOnce).be.true;
+             should(res.render.firstCall.args[1]).containDeep(respond);
              
+
+                });
+           });
+
+       
+         });
+         
+    
+     
+          
+             
+             
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    
+     describe.only('Тестирование метода saved', () => {
+              
+             /*
+              * обращаемся к методу
+              * метод должен обратиться к базе данных
+              * и метод должен  найти заданный id и обновить данные в базе и сообщить статус - ок или ошибка.
+              * 
+              */
+             
+              afterEach(() => {
+              
+                 Worker.WorkerSaved.restore();
+                 
+             
+             });
+        
+          describe("Testing method 1 - return ok", () => {
+        
+            before(() => {
+                 
+                 const callback = 'ok';
+                 const err = null;
+                 
+                   let req = { 
+                             params: {
+                                       id: 1,
+                                       body: { 
+                                           name: 'name' 
+                                       }
+                             }
+                          };  
+
+
+                                
+                 const stub = sinon
+                            .stub(Worker, 'WorkerSaved')
+                            .withArgs(req.params.id, req.params.body)
+                            .yields(callback, err);
+                            //.yields(callback);
+                   
+                 });
+
+
+                it ('Стандартное поведение', function() {
+                 /*
+                  * обращаемся к методу c аргументом id и данными
+                  * метод должен обратиться к базе данных
+                  * и метод должен записать данные и вернуть статус сообщения - ок или ошибка.
+                   */
+
+                 
+                  let respond = {
+                                   status: 'Данный обновлены', info: 'ok' 
+                               };
+
+                      
+                  
+                 
+                 let req = { 
+                             params: {
+                                       id: 1,
+                                       body: { 
+                                           name: 'name' 
+                                       }
+                             }
+                          };
+                 
+                 
+                                                   
+                                  
+                 let res = {
+                     render: sinon.spy()
+                 };
+                 
+                 controller.saved(req, res);
+                 
+                 
+                 should(res.render.calledOnce).be.true;
+                 
+                 
+                 should(res.render.firstCall.args[1]).containDeep(respond);
+               
+                    
+                 });
+
+             });
+         
+
+          describe("Testing create method 2", () => {
+         
+             before(() => {
+                     const err = 'error';
+                     const data = {};
+                     const stub = sinon
+                            .stub(Worker, 'WorkerSaved')
+                            .yields(data, err);
+                    
+             });
+         
+               it ('возвращается error  из БД - поведение', function() {
+             /*
+              * обращаемся к методу
+              * метод должен обратиться к базе данных
+              * и метод должен вернуть данные из базы -курсор и статус сообщения - ок или ошибка.
+              * 
+              */
+                         
+                 let req = { };
+                 let res = {
+                     render: sinon.spy()
+                 };
+                 
+                 const respond = { data: null, status: "Error response Database" };
+             
+             
+             controller.index(req, res);
+                      
+          
+             should(res.render.calledOnce).be.true;
+             should(res.render.firstCall.args[1]).containDeep(respond);
+             
+
+                });
+           });
+
+       
              
              
              
          });
-    });
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+        
+
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     
      describe('Тестирование метода open', () => {
-               
         
-         it ('Стандартное поведение', function() {
              /*
               * обращаемся к методу
               * метод должен обратиться к базе данных
@@ -229,15 +401,11 @@ describe("Test модуля worker.controller", function() {
               */
              
              
-             controller.open(res, req);
-             
-             
              
              
          });
-    });
+   
     
-    
-    
+ //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 });
 
